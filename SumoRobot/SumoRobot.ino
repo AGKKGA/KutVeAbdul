23f700ceb28121c815e21db83c9216ddc78911d5
mktookyo <mktookyoo@gmail.com>
2024-05-18 00:08:28 +0300

    Adding a 3+ US sensors working algorithm

--- SumoRobot/SumoRobot.ino
+++ SumoRobot/SumoRobot.ino
@@ -20,24 +20,34 @@ int In1L = 7;  // Input 1 for the left DC motor to control the direction of the 
 int In2L = 8;  // Input 2 for the left DC motor to control the direction of the motor
 int EnL = 9;   // Enable pin for the left DC motor to control the speed of the motor
 
+// Pins for 3 ultrasonic sensors 
+int USS_trig[] = {13,15,16}; // Trigger for each sensor {left, front, right} NEED TO CHANGE PINS
+int USS_echo[] = {17,18,19}; // Echo pins for each sensor {left front, right} NEED TO CHANGE PINS
+// distance of each sensor showing
+int USS_dist[] = {0,0,0}; // Distance value for each sensor {left, front, right}
+
 // Setup
 void setup() {
   Serial.begin(9600);
   pinMode(HC_TRIG, OUTPUT); // trig output
   pinMode(HC_ECHO, INPUT);  // echo input
 }
-// Function for measuring distance in cm by UltraSonic Sensor
-float getDist() {
+// Function for measuring distance in cm by UltraSonic Sensor 
+float getDist(int trig, int echo) {
   // Impulse 10 Microseconds
-  digitalWrite(HC_TRIG, HIGH);
+  digitalWrite(trig, HIGH);
   delayMicroseconds(10);
-  digitalWrite(HC_TRIG, LOW);
+  digitalWrite(trig, LOW);
   // Measuring time of signal treveling 
-  uint32_t us = pulseIn(HC_ECHO, HIGH);
+  uint32_t us = pulseIn(echo, HIGH);
   // Calculating distance (in cm) and returning
   return (us * 0.034 / 2); 
 }
-
+void dist_update(){ //this function
+  for (int i=0; i<=2; i++){
+    USS_dist[i]=getDist(USS_trig[i],USS_echo[i]);
+  }
+}
 //foward function:
 void goForward() {
   digitalWrite(In1R, HIGH);  // It is high and the other one is low so the motor will move clockwise
@@ -108,7 +118,7 @@ bool floor(uint32_t sensor){ //Function for QTR sensor
   }
 }
 // Main loop ////////////////////////////////////////////////////////////
-void loop() {  
-  
+void loop() {   
+
 }
 
