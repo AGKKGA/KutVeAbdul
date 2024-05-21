cefe12744a45dc1fb69921a831362dd8ba269acd
mktookyo <mktookyoo@gmail.com>
2024-05-21 20:24:50 +0300

    Update SumoRobot.ino
    Adding: checklist before match
    Removed: 3 Ultrasonic sensor function, we will use only 1.

--- SumoRobot/SumoRobot.ino
+++ SumoRobot/SumoRobot.ino
@@ -1,6 +1,12 @@
 /* Some important info:
 Wheels diameter without band: 64mm
 Wheels diameter with band: 67mm
+Wheels widht: 26mm
+Formula for movement: 
+*/
+/* Check list before match:
+1. Wash Tires and dry it
+2. Check Voltage in batteries.
 */
 
 
@@ -14,8 +20,8 @@ int QTR_B = 6;  // The BACK QTR-1A sensor, we will use it as a digital pin
 
 //Ultrasonic
 // Vcc->5V 
-int USS_trig[] = {13,15,16}; // Trigger for each sensor {left, front, right} NEED TO CHANGE PINS
-int USS_echo[] = {17,18,19}; // Echo pins for each sensor {left front, right} NEED TO CHANGE PINS
+int USS_trig; // Trigger pin
+int USS_echo; // Echo pin
 
 // Motor (Right)
 int In1R = 10;  // Input 1 for the right DC motor to control the direction of the motor
@@ -27,22 +33,15 @@ int In1L = 7;  // Input 1 for the left DC motor to control the direction of the 
 int In2L = 8;  // Input 2 for the left DC motor to control the direction of the motor
 int EnL = 9;   // Enable pin for the left DC motor to control the speed of the motor
 
-// Pins for 3 ultrasonic sensors 
-
-
 // VARIABLES
-int USS_dist[] = {0,0,0}; // Distance value for each sensor {left, front, right}
+
 
 // Setup
 void setup() {
   Serial.begin(9600);
   //Ultrasonic sensor
-  for(int i=0; i<=2; i++){
-    pinMode(USS_trig[i], OUTPUT); // trig output
-  }
-  for (int i=0; i<=2; i++){
-    pinMode(USS_echo[i], INPUT);  // echo input
-  }
+  pinMode(USS_trig, OUTPUT);
+  pinMode(USS_echo, INPUT);
   // QTR
   pinMode(QTR_F, INPUT);  // set the FRONT egde senosr as an INPUT
   pinMode(QTR_B, INPUT);  // set the BACK egde senosr as an INPUT
@@ -60,20 +59,15 @@ void setup() {
   analogWrite(EnL, 0);  // Stop the left DC motor by making the speed 0
 }
 // Function for measuring distance in cm by UltraSonic Sensor 
-float getDist(int trig, int echo) {
+float getDist() {
   // Impulse 10 Microseconds
-  digitalWrite(trig, HIGH);
+  digitalWrite(USS_trig, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trig, LOW);
+  digitalWrite(USS_trig, LOW);
   // Measuring time of signal treveling 
-  uint32_t us = pulseIn(echo, HIGH);
+  uint32_t us = pulseIn(USS_echo, HIGH);
   // Calculating distance (in cm) and returning
   return (us * 0.034 / 2); 
-}
-void dist_update(){ //this function
-  for (int i=0; i<=2; i++){
-    USS_dist[i]=getDist(USS_trig[i],USS_echo[i]);
-  }
 }
 //foward function:
 void goForward() {
@@ -135,12 +129,12 @@ void stop() {
   digitalWrite(In2L, HIGH);
   analogWrite(EnL, 255);  // we can write 0 instead of 255 but since the both pins in this motor are high, the motor will stop
 }
-bool floor(uint32_t sensor){ //Function for QTR sensor
-  if (sensor=="front"){
+bool floor(uint32_t cmd){ //Function for QTR sensor
+  if (cmd=="front"){
     // has to be a function that uses FRONT qtr sensor
     
   }
-  else if(sensor=="back"){
+  else if(cmd=="back"){
     // has to be a function that uses BACK qtr sensor
   }
 }
