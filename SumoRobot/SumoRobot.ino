f91cbee4ffa0b5c5124e19d52f22c2f3600992dd
mktookyo <mktookyoo@gmail.com>
2024-05-29 03:57:17 +0300

    Update SumoRobot.ino
    Working code, basic turning

--- SumoRobot/SumoRobot.ino
+++ SumoRobot/SumoRobot.ino
@@ -69,9 +69,8 @@ void setup() {
     delay(1);
   }
   digitalWrite(13, LOW);
-  // QTR pinMode is adjusted in function
   delay(2000);
-  // angle_turnRight(90);
+
 }
 // Function for measuring distance in cm by UltraSonic Sensor 
 float getDist() {
@@ -116,15 +115,16 @@ void turnLeft(int percent=100) {  //percent value from 0 to 100, for power of mo
 }
 
 // backwards
-void goBackward() {
+void goBackward(int percent = 100) {
+  int power = ceil(2.55*percent);
   digitalWrite(In1R, LOW);  // It is low and the other one is high so the motor will move
   // counterclockwise
   digitalWrite(In2R, HIGH);
   digitalWrite(In1L, LOW);  // It is low and the other one is high so the motor will move
   // counterclockwise
   digitalWrite(In2L, HIGH);
-  analogWrite(EnR, 255);    // move at full speed
-  analogWrite(EnL, 255);  // move at full speed
+  analogWrite(EnR, power);    // move at full speed
+  analogWrite(EnL, power);  // move at full speed
 }
 
 //rotate
@@ -138,48 +138,28 @@ void rotate() {
 }
 
 // stop
-void stop() {
-  digitalWrite(In1R, HIGH);  // It is high and the other one is low so the motor will move clockwise
-  digitalWrite(In2R, HIGH);
-  digitalWrite(In1L, HIGH);  // It is high and the other one is high so the motor will stop
-  digitalWrite(In2L, HIGH);
-  analogWrite(EnR, 255);     // move at full speed
-  analogWrite(EnL, 255);  // we can write 0 instead of 255 but since the both pins in this motor are high, the motor will stop
-}
-void turn_off(){
+void turn_off(int time = 100){
   digitalWrite(In1R, LOW);  // It is high and the other one is low so the motor will move clockwise
   digitalWrite(In2R, LOW);
   digitalWrite(In1L, LOW);  // It is high and the other one is high so the motor will stop
   digitalWrite(In2L, LOW);
-  delay(10);
-}
-bool floorBlack(){ //Function for QTR sensor, detecting black side. (Return "true", when sees black)
-  // I've tried to understand how this code works, but couldnt, but it works, all we need XD. The first rule of engineering: if it works, dont touch it. (Only god and programmer of this code, knows how this code works)
-  // Emitting the IR signal for 1ms
-  pinMode(QTR_output, OUTPUT);
-  digitalWrite(QTR_output,HIGH);
-  delay(5);
-  pinMode(QTR_output, INPUT); // changing pinmode of sensor (it's really genius how this sensor works ha)
-  QTR_time_1=micros(); // measuring time when signal emitted
-  // Recieving the IR signal
-  while (digitalRead(QTR_output)==HIGH){ 
-    delayMicroseconds(5);
-  }
-  QTR_time_2=micros();  
-  color_val=QTR_time_2-QTR_time_1; 
-  Serial.println(color_val); // printing to serial monitor (optional)
-  if (color_val>WHITE){ // return true, if material is dark
-    return (true);
-  }
-  else{
-    return (false); // return false, if material is bright
-  }
+  analogWrite(EnR, 0);
+  analogWrite(EnL, 0);
+  delay(time);
 }
 
-void search(){ //searching for opponent
+void search(bool cmd=true){ //searching for opponent
   bool found = false;
-  turnRight(50);
+  if (cmd==true){
+    turnRight(50); 
+  }
+  else{
+    turnLeft(50);
+  }
   while (found==false){
+    Serial.println("Searching function working!");
+    Serial.print("Current distance: ");
+    Serial.println(getDist());
     if (getDist()<=distance_of_opponent){
       opponent=true;
       found=true;
@@ -187,105 +167,81 @@ void search(){ //searching for opponent
     else{
       found=false;
     }
-    delay(50);
+    // delay(50);
   }
-  stop();
+  turn_off();
 }
 
-void attack(){
-  bool opponent_now;
-  unsigned long t_1, t_2;
-  float time_for_turning = 120;
-  unsigned long turning_time = 0;
-  bool found;
-  if (opponent==false){
-    search();
-    stop();
-    delay(100);
-  }
-  else{
-    if (direction==true){
-      turnRight(50);
-      t_1=millis();
-      while (turning_time<time_for_turning){
-        if (getDist()<distance_of_opponent){
-          opponent_now = true;
-          direction = true;
-          turn_off();
-          // delay(100);
-          turnLeft();
-          delay(120);
-          turn_off();
-          break;
-        }
-        else{
-          t_2=millis();
-          turning_time = t_2-t_1;
-        }
-      }
-    }
-    else{
-      turnLeft(50);
-      t_1=millis();
-      while (turning_time<time_for_turning){
-        if (getDist()<distance_of_opponent){
-          opponent_now = true;
-          direction = false;
-          // stop();
-          turn_off();
-          // delay(100);
-          turnRight();
-          delay(120);
-          turn_off();
-          break;
-        }
-        else{
-          t_2=millis();
-          turning_time = t_2-t_1;
-        }
-      }
-    }
-    goForward();
-    while (opponent_now == true){
-      if (getDist()<distance_of_opponent){
-        opponent_now = true;
-      }
-      else{
-        opponent_now = false;
-        opponent = true;
-      }
-    }
-    stop();
-  }
-}
 // Main loop ////////////////////////////////////////////////////////////
 void loop() {
-  // while (true){
-  //   if(getDist()>70){
-  //     search();
-  //   }
-  //   else{
-  //     int dista=getDist();
-      // turn_off();
-      // // delay(100);
-      // turnLeft();
-      // delay(120);
-      // turn_off();
-  //     while(getDist()<70){
-  //       goForward();
-  //     }
-  //   }
-  // }
-  // turnRight(50);
-  // goForward();
-  turnRight();
-  delay(1000);
-  digitalWrite(13, HIGH);
-  turnLeft();
-  delay(30);
-  turn_off();
-  delay(2000);
-  digitalWrite(13, LOW);
+  while (true){
+    unsigned long time = millis();
+    int counter = 1;
+    if(counter==1){
+      if(getDist()>distance_of_opponent){
+        Serial.println("Searching!");
+        turn_off();
+        search();
+      }
+      else{
+        int dista=getDist();
+        Serial.println("Found something! 1st loop, attacking");
+        Serial.print("distance: ");
+        Serial.println(getDist());
+
+        while(getDist()<distance_of_opponent){
+          Serial.println("Attacking!");
+          Serial.print("distance: ");
+          Serial.println(getDist());
+          goForward();
+        }
+      }
+      counter+=1;
+    }
+    else{
+      if(getDist()>distance_of_opponent){
+        Serial.println("Searching!");
+        turn_off();
+        search();
+      }
+      else{
+        int dista=getDist();
+        Serial.println("Found something!");
+        Serial.print("distance: ");
+        Serial.println(getDist());
+        turn_off();
+        if (getDist()<distance_of_opponent){
+          while(getDist()<distance_of_opponent){
+            Serial.println("Attacking!");
+            Serial.print("distance: ");
+            Serial.println(getDist());
+            goForward();
+          }
+        }
+        else{
+          Serial.println("Turning!");
+          turnLeft();
+          if (dista>30){
+            delay(120);
+          }
+          else{
+            delay(120);
+          }
+          // delay(100);
+          turn_off(50);
+          Serial.println("Turned, starting attack...");
+          while(getDist()<distance_of_opponent){
+            Serial.println("Attacking!");
+            Serial.print("distance: ");
+            Serial.println(getDist());
+            goForward();
+          }
+        }
+      }
+    }
+    Serial.print("Time taked for one loop: ");
+    Serial.println(millis()-time);
+  }
 }
 
 
